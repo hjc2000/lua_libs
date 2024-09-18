c92d934865106274c49714510a817909ac03f01b
hjc2000 <hjc.666@foxmail.com>
2024-09-18 13:21:31 +0800

    Servo.Core

---
+++ Servo/Core.lua
@@ -0,0 +1,17 @@
+if (Servo == nil) then
+	Servo = {}
+end
+
+if (Servo.Core == nil) then
+	Servo.Core = {}
+end
+
+
+
+
+
+
+--- 重启伺服
+function Servo.Core.Restart()
+	Servo.Param.Set(3, 98, 9999)
+end
