4160ede4dce1487bb90c43d1956d1e284105d5a4
hjc2000 <hjc.666@foxmail.com>
2024-09-18 14:19:52 +0800

    Mode7

--- Servo/Mode7.lua
+++ Servo/Mode7.lua
@@ -59,7 +59,7 @@ end
 function Servo.Mode7.ChangeToPositionMode()
 	Servo.EI.SetForwardSignal(false)
 	Servo.EI.SetReverseSignal(false)
-	Servo.CancelPositioning()
+	Servo.Mode7.CancelPositioning()
 
 	Servo.EI.Set(12, false)
 end
@@ -68,7 +68,7 @@ end
 function Servo.Mode7.ChangeToSpeedMode()
 	Servo.EI.SetForwardSignal(false)
 	Servo.EI.SetReverseSignal(false)
-	Servo.CancelPositioning()
+	Servo.Mode7.CancelPositioning()
 
 	Servo.EI.Set(12, true)
 	Servo.EI.Set(13, false)
@@ -78,10 +78,58 @@ end
 function Servo.Mode7.ChangeToTorqueMode()
 	Servo.EI.SetForwardSignal(false)
 	Servo.EI.SetReverseSignal(false)
-	Servo.CancelPositioning()
+	Servo.Mode7.CancelPositioning()
 
 	Servo.EI.Set(12, true)
 	Servo.EI.Set(13, true)
+end
+
+--- 设置转速并按此转速运行。
+--- 可以为正负数或 0。设置为正数则正转，负数则反转，0 则停止。
+--- 单位：rpm。
+--- @param value number 浮点数。
+function Servo.Mode7.SetSpeedAndRun(value)
+	-- 要设置的速度如果和当前转速反向了，先断开正转、反转信号。
+	if (Servo.Feedback.Speed() * value < 0) then
+		Servo.EI.SetForwardSignal(false)
+		Servo.EI.SetReverseSignal(false)
+	end
+
+	Servo.Param.SetMaxSpeedInPositonAndSpeedControlMode(value)
+	AXIS_SPEED(value)
+
+	if (value > 0) then
+		-- 正转
+		Servo.EI.SetForwardSignal(true)
+		Servo.EI.SetReverseSignal(false)
+	elseif (value < 0) then
+		-- 反转
+		Servo.EI.SetForwardSignal(false)
+		Servo.EI.SetReverseSignal(true)
+	end
+end
+
+--- 设置绝对定位立即数并运行。
+--- @param value integer
+function Servo.Mode7.SetAbsolutePositionAndRun(value)
+	AXIS_MOVEABS(value)
+	Servo.EI.TriggerRisingEdge(14)
+end
+
+--- 通过 EI 进行位置预置，设置零点
+function Servo.Mode7.PresetPosition()
+	Servo.EI.TriggerRisingEdge(15)
+end
+
+--- 取消定位
+function Servo.Mode7.CancelPositioning()
+	Servo.EI.TriggerRisingEdge(16)
+end
+
+--- 停止转动，将速度锁定为 0. 无论原来是什么模式（定位模式、速度模式、转矩模式）都可以使用。
+function Servo.Mode7.Stop()
+	Servo.Mode7.ChangeToSpeedMode()
+	Servo.Mode7.SetSpeedAndRun(0)
 end
 
 ---------------------------------------------------------------------------------------------
