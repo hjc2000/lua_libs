1dffa30b948f91bc5b8a7d7bdec4d06c6d8f0596
hjc2000 <hjc.666@foxmail.com>
2024-09-18 13:19:12 +0800

    Servo.EO

---
+++ Servo/EO.lua
@@ -0,0 +1,29 @@
+if (Servo == nil) then
+	Servo = {}
+end
+
+if (Servo.EO == nil) then
+	Servo.EO = {}
+end
+
+
+
+
+
+--- 获取输出端子的值
+--- @param index any
+--- @return boolean
+function Servo.EO.Get(index)
+	return SRV_EOUT(index) ~= 0
+end
+
+--- 设置 EO 的值。
+--- @param index integer EO 的索引。
+--- @param value boolean
+function Servo.EO.Set(index, value)
+	if (value) then
+		SRV_EOUT(index, 1)
+	else
+		SRV_EOUT(index, 0)
+	end
+end
