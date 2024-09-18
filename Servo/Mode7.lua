0c138b999748facda723fad79494eea154bcec24
hjc2000 <hjc.666@foxmail.com>
2024-09-18 13:52:57 +0800

    function Servo.Mode7.Config()

---
+++ Servo/Mode7.lua
@@ -0,0 +1,56 @@
+if (Servo == nil) then
+	Servo = {}
+end
+
+if (Servo.Mode7 == nil) then
+	Servo.Mode7 = {}
+end
+
+--- 配置成模式 7
+--- @note 会占用一些通信 EI。
+function Servo.Mode7.Config()
+	--- 将 EI 9 配置为使能信号
+	local function ConfigEI()
+		if (Servo.Param.Get(3, 1) == 1) then
+			-- 默认状态下，EI1 是伺服使能，想要让 EI9 是伺服使能，需要释放 EI1
+			Servo.Param.Set(3, 1, 0)
+		end
+
+		-- EI9 配置为使能
+		Servo.Param.Set(3, 9, 1)
+
+		-- EI10
+		-- 正转指令
+		Servo.Param.Set(3, 10, 2)
+
+		-- EI11
+		-- 反转指令
+		Servo.Param.Set(3, 11, 3)
+
+		-- EI12
+		-- 控制模式切换
+		-- OFF 是位置控制。ON 后可通过 EI13 进一步选择速度控制和转矩控制。
+		Servo.Param.Set(3, 12, 36)
+
+		-- EI13
+		-- 转矩控制、速度控制切换
+		-- EI12 为 ON 后，EI13 为 OFF 则是速度控制，为 ON 则是转矩控制。
+		Servo.Param.Set(3, 13, 38)
+
+		-- EI14
+		-- 定位数据启动
+		Servo.Param.Set(3, 14, 4)
+
+		-- EI15
+		-- 位置预置
+		Servo.Param.Set(3, 15, 16)
+
+		-- EI16
+		-- 定位取消
+		Servo.Param.Set(3, 16, 32)
+	end
+
+	-- 配置成模式 7
+	Servo.Param.Set(1, 1, 7)
+	ConfigEI()
+end
