d9aea10010b8ae32af999e61a165769c23780182
hjc2000 <hjc.666@foxmail.com>
2024-09-18 17:21:56 +0800

    function Servo.Feedback.Position()

--- Servo/Feedback.lua
+++ Servo/Feedback.lua
@@ -16,3 +16,10 @@ end
 function Servo.Feedback.Speed()
 	return SRV_MON(0)
 end
+
+--- 反馈位置。能被位置预置清 0。此外还受参数中设置的 “编码器旋转一圈的脉冲数” 影响。
+--- 这个参数设置为多少，旋转一圈本属性就变化多少。
+--- @return integer
+function Servo.Feedback.Position()
+	return SRV_MON(6)
+end
