cb263c472ef712c7d0859bc6f4a7b69a255a4d91
hjc2000 <hjc.666@foxmail.com>
2024-09-25 14:50:06 +0800

    function Servo.Param.Set(group, index, value, write_to_eerom)

--- Servo/Param.lua
+++ Servo/Param.lua
@@ -7,11 +7,6 @@ if (true) then
 		Servo.Param = {}
 	end
 
-
-
-
-
-
 	--- 获取伺服参数
 	--- @param group integer 组索引
 	--- @param index integer 参数子索引
@@ -24,8 +19,14 @@ if (true) then
 	--- @param group integer 组索引
 	--- @param index integer 参数子索引
 	--- @param value number 参数值
-	function Servo.Param.Set(group, index, value)
-		SRV_PARA(group, index, value)
+	--- @param write_to_eerom boolean|nil 是否将参数写入 EEROM。
+	--- 传入 nil 或 true 则表示要写入。只有传入 false 时才会不写入。
+	function Servo.Param.Set(group, index, value, write_to_eerom)
+		if (write_to_eerom == false) then
+			SRV_PARA(group, index, value, 0)
+		else
+			SRV_PARA(group, index, value)
+		end
 	end
 
 	--- 转矩限制
@@ -33,25 +34,25 @@ if (true) then
 		--- 正转转矩限制。单位：额定输出转矩的百分比。
 		--- @return integer
 		function Servo.Param.ForwardTorqueLimit()
-			return SRV_PARA(1, 27)
+			return Servo.Param.Get(1, 27)
 		end
 
 		--- 设置正转转矩限制。单位：额定输出转矩的百分比。
 		--- @param value integer
 		function Servo.Param.SetForwardTorqueLimit(value)
-			SRV_PARA(1, 27, value, 0)
+			Servo.Param.Set(1, 27, value, false)
 		end
 
 		--- 反转转矩限制。单位：额定输出转矩的百分比。
 		--- @return integer
 		function Servo.Param.ReverseTorqueLimit()
-			return SRV_PARA(1, 28)
+			return Servo.Param.Get(1, 28)
 		end
 
 		--- 设置反转转矩限制。单位：额定输出转矩的百分比。
 		--- @param value integer
 		function Servo.Param.SetReverseTorqueLimit(value)
-			SRV_PARA(1, 28, value, 0)
+			Servo.Param.Set(1, 28, value, false)
 		end
 
 		--- 同时设置正转和反转的转矩限制。单位：额定输出转矩的百分比。
@@ -77,7 +78,7 @@ if (true) then
 	--- 设置伺服旋转一周的指令脉冲数
 	--- @param value integer
 	function Servo.Param.SetPulseCountPerCircle(value)
-		Servo.Param.Set(1, 5, value)
+		Servo.Param.Set(1, 5, value, false)
 	end
 
 	--- 转速限制
@@ -91,7 +92,7 @@ if (true) then
 		--- 设置位置控制和速度控制时的最大转速。单位：rpm
 		--- @param value integer
 		function Servo.Param.SetSpeedLimit(value)
-			Servo.Param.Set(1, 25, value)
+			Servo.Param.Set(1, 25, value, false)
 		end
 	end
 end
