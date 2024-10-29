6d9bbc9c22ba98b7387e2ef57ac4c2c7ab28f65c
hjc2000 <hjc.666@foxmail.com>
2024-10-29 10:49:56 +0800

    MaxStaticFrictionCalculator

--- Calculator/MaxStaticFrictionCalculator.lua
+++ Calculator/MaxStaticFrictionCalculator.lua
@@ -3,15 +3,15 @@ if true then
 		Calculator = {}
 	end
 
-	if (Calculator.StaticFrictionCalculator == nil) then
+	if (Calculator.MaxStaticFrictionCalculator == nil) then
 		--- 最大静摩擦计算器。
-		Calculator.StaticFrictionCalculator = {}
+		Calculator.MaxStaticFrictionCalculator = {}
 	end
 
 	--- 构造一个新的最大静摩擦计算器。
 	--- @param f_max number 光轴时的最大静摩擦。
 	--- @param mu number 摩擦系数。
-	function Calculator.StaticFrictionCalculator.New(f_max, mu)
+	function Calculator.MaxStaticFrictionCalculator.New(f_max, mu)
 		local context = {}
 		context.f_max = f_max
 		context.mu = mu
@@ -20,7 +20,7 @@ if true then
 	--- 计算当前的最大摩擦力。
 	--- @param context table
 	--- @param weight number 当前施加到轴上的重量或压力。单位：N.
-	function Calculator.StaticFrictionCalculator.Calculate(context, weight)
+	function Calculator.MaxStaticFrictionCalculator.Calculate(context, weight)
 		return context.f_max + context.mu * weight
 	end
 end
